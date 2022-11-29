744e43d64c99f51ec9002de552ead600568ac127
Varmais1 <418487@bsd48.org>
2022-11-29 14:25:43 -0800

    Final Version Working without comments

--- Classes.cpp
+++ Classes.cpp
@@ -12,10 +12,10 @@ void lowercase(char sentence[]);
 
 
 int main() {
-  char* publisher;
-  char* title;
-  char* artist;
-  char* director;
+  char publisher[150];
+  char title[150];
+  char artist[150];
+  char director[150];
   int year;
   char type[20];
   double duration;
@@ -24,57 +24,68 @@ int main() {
   bool run = true;
   char command[10] = {'d','e','f','a','u','l','t'};
   while(run == true) {
-    cout << "Type add if you wish to add a form of media, search if you wish to find a work, and quit if you wish to quit." << endl;
+    cout << "Type add if you wish to add a form of media, search if you wish to find a work, delete in order to delete a work, and quit if you wish to quit." << endl;
     lowercase(command);
-    cin >> command;
+    cin.get(command,150);
+    cin.ignore();
     cout << endl;
     if(strcmp(command, "add") == 0) {
       cout << "What type of media would you like to add?" << endl;
-      cin >> type;
+      cin.get(type,20);
+      cin.get();
       lowercase(type);
-      if(strcmp(type, "videogames") == 0 || strcmp(type, "videogame")) {
+      if(strcmp(type, "videogames") == 0 || strcmp(type, "videogame") == 0) {
 	cout << "What is the title of this videogame?" << endl;
 	cin.get(title, 150);
 	cin.get();
 	cout << "What was the year that this videogame was released? (please type in a whole number)" << endl;
 	cin >> year;
+	cin.ignore();
 	cout << "Who was the publisher of this videogame?" << endl;
 	cin.get(publisher, 150);
-	cin.get();
+	cin.ignore();
 	cout << "What is the rating of this videogame? (please type in a real number)" << endl;
+	cin >> rating;
+	cin.ignore();
 	videogames* games = new videogames(title,year,publisher,rating);
 	media.push_back((generalMedia*)games);
+	strcmp(command, "          ");
       }
       else if(strcmp(type, "music") == 0) {
 	cout << "What is the title of this piece of music?" << endl;
 	cin.get(title, 150);
-	cin.get();
+	cin.ignore();
 	cout << "Who was the artist who made this piece of music?" << endl;
 	cin.get(artist, 150);
-	cin.get();
+	cin.ignore();
 	cout << "Which year was this song released? (please type in a whole number)" << endl;
 	cin >> year;
+	cin.ignore();
 	cout << "What is the duration of this song in seconds? (please type in a real number)" << endl;
 	cin >> duration;
+	cin.ignore();
 	cout << "Who is the publisher?" << endl;
 	cin.get(publisher, 150);
-	cin.get();
+	cin.ignore();
 	music* work = new music(title, year, artist, duration, publisher);
 	media.push_back((generalMedia*)work);
       }
-      else if(strcmp(type, "movies") == 0) {
+      else if(strcmp(type, "movies") == 0 || strcmp(type, "movie") == 0) {
 	cout << "What is the title of this movie?" << endl;
 	cin.get(title, 150);
-	cin.get();
+	cin.ignore();
 	cout << "Who is the director of this movie?" << endl;
 	cin.get(director, 150);
-	cin.get();
+	cin.ignore();
 	cout << "Which year was this movie made?" << endl;
 	cin >> year;
+	cin.ignore();
 	cout << "What is the duration of this movie in minutes?" << endl;
 	cin >> duration;
+	cin.ignore();
 	cout << "What is the current rating of this movie?" << endl;
 	cin >> rating;
+	cin.ignore();
 	movies* movie = new movies(title, year, director, duration, rating);
 	media.push_back((generalMedia*)movie);
       }
@@ -82,7 +93,7 @@ int main() {
     else if(strcmp(command, "search") == 0) {
       cout << "What is the title of the media you want to find?" << endl;
       cin.get(title, 150);
-      cin.get();
+      cin.ignore();
       for(auto i = media.begin(); i !=media.end(); i++) {
 	if(strcmp(title, (*i)->getTitle()) == 0) {
           (*i)->print();
@@ -92,16 +103,30 @@ int main() {
     
       
     }
+    else if(strcmp(command, "delete") == 0) {
+      cout << "What is the title of the media you wish to delete?" << endl;
+      cin.get(title, 150);
+      cin.ignore();
+      for(auto i = media.begin(); i != media.end(); i++) {
+	if(strcmp(title, (*i)->getTitle()) == 0) {
+	  delete *i;
+	  media.erase(i);
+	  break;
+	}
+      }
+    }
     else if(strcmp(command, "quit") == 0) {
       for(auto i = media.begin(); i!= media.end(); i++) {
 	delete *i;
       }
       run = false;
     }
-
+    
+    strcmp(command, "          ");
 
 
   }
+  //used to test if the videogames and generalMedia classes were working.
   /*strcpy(title, "game");
   strcpy(publisher, "Nintendo");
   videogames a = videogames(title, 2005, publisher, 4.90);
